883028480f292bea3173aadf310b8473dfff4bb8
Oliver Wilkins <okwilkins@googlemail.com>
2022-08-16 16:09:23 +0000

    Part one speed improvements

--- revival-of-code/day-5/src/main.rs
+++ revival-of-code/day-5/src/main.rs
@@ -1,4 +1,4 @@
-use fancy_regex::Regex;
+use regex::Regex;
 use std::fmt::Debug;
 
 fn main() {
@@ -33,13 +33,44 @@ fn parse_args() -> Arguments {
 }
 
 fn detect_nice_text(input: &str) -> bool {
+    // Manually create alpha chars for Regex as this can avoid backreferences
+    // which is very bad for performance
+    let lower_alphas = (b'a'..=b'z')
+        .map(|c| format!("{}{}", c as char, c as char))
+        .collect::<Vec<_>>();
+    let mut lower_alpha_match = false;
+    
+    let vowels = vec!["a", "e", "u", "i", "o"];
+    let mut vowels_count: usize = 0;
+
+    let bad_patterns = vec!["ab", "cd", "pq", "xy"];
+    let mut bad_match = false;
+
+    for lower_alpha in lower_alphas {
+        if input.matches(lower_alpha.as_str()).count() >= 1 {
+            lower_alpha_match = true;
+        }
+    }
+
+    for vowel in vowels {
+        vowels_count += input.matches(vowel).count();
+    }
+
+    for bad_pattern in bad_patterns {
+        if input.matches(bad_pattern).count() >= 1 {
+            bad_match = true;
+        }
+    }
+
+    lower_alpha_match && vowels_count >= 3 && !bad_match
+}
+
+fn improved_detect_nice_text(input: &str) -> bool {
     let good_re_1 = Regex::new(r"([a-zA-Z])\1").unwrap();
     let good_re_2 = Regex::new(r"(\w*[aeuio]\w*){3,}").unwrap();
     let bad_re = Regex::new(r"ab|cd|pq|xy").unwrap();
 
-    good_re_1.is_match(input).unwrap()
-        && good_re_2.is_match(input).unwrap()
-        && !bad_re.is_match(input).unwrap()
+    good_re_1.is_match(input) && good_re_2.is_match(input) && !bad_re.is_match(input)
 }
 
 #[test]
